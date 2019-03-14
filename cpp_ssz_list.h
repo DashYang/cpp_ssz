9aed2c06761bee3fc757f3d4afb87144140b51e2
SigmoiD <alphabet@hotmail.co.kr>
2019-03-15 00:52:04 +0900

    barely support list encoding
    Signed-off-by: SigmoiD <alphabet@hotmail.co.kr>

---
+++ cpp_ssz_list.h
@@ -0,0 +1,81 @@
+/*
+	This file is part of cpp_ssz.
+    @author SigmoiD <alphabet@hotmail.co.kr>
+*/ 
+
+#ifndef __CPP_SSZ_LIST_H__
+#define __CPP_SSZ_LIST_H__
+
+#include "Common.h"
+
+namespace ssz {
+
+template<class T>
+class List
+{
+protected:
+	unsigned int m_size;
+	std::vector<T> m_data;
+public:
+	List() 
+	{ 
+	}
+
+	~List()
+	{
+		m_data.empty();
+	}
+
+	unsigned int size() const { return m_data.size(); }
+	std::vector<T>& data() { return m_data; }
+
+// encode/decode section
+    void from_bytes(const bytes& data, byteorder bo);
+	bytes to_bytes(unsigned int size, byteorder bo);
+/*
+// operators
+	bool operator==(const bytesN<N>& b)
+	{
+	     return this->m_data == b.get_data_array();
+	}
+*/
+};
+
+template<class T>
+void List<T>::from_bytes(const bytes& data, byteorder bo)
+{
+/*
+	int prefix = 0;
+    prefix |= data[0] << 16;
+	prefix |= data[1] << 8;
+	prefix |= data[2] << 0;
+    prefix -= 8388608;
+
+    for(int i=0; i< prefix; i++)
+        m_data[i] = data[3+i];
+*/
+}
+
+template<class T>
+bytes List<T>::to_bytes(unsigned int size, byteorder bo)
+{
+
+    bytes temp1;
+    for (unsigned i=0; i < m_data.size(); i++) {
+        bytes t = m_data[i].to_bytes(m_data[i].size(), little);
+        temp1.insert(temp1.end(), t.begin(), t.end()); 
+    }
+
+	int prefix = 8388608 + temp1.size();
+    bytes temp;
+    temp.push_back((prefix & 0x00ff0000) >> 16);
+	temp.push_back((prefix >> 8) & 0xff);
+	temp.push_back((prefix >> 0) & 0xff);
+    temp.insert(temp.end(), temp1.begin(), temp1.end());
+	return bytes(temp); 
+}
+
+
+
+}
+#endif
---
+++ list_test.cpp
@@ -0,0 +1,54 @@
+/*
+	This file is part of cpp_ssz.
+    @author SigmoiD <alphabet@hotmail.co.kr>
+*/ 
+
+#include "Common.h"
+#include "cpp_ssz_bytes.h"
+#include "cpp_ssz_uint.h"
+#include "cpp_ssz_list.h"
+using namespace std;
+using namespace ssz;
+
+//pretty printer section
+void print_hex(const bytes& a) {
+		for(int i=0; i<a.size(); i++)
+			printf("%02x", a.data()[i]);
+		printf("\n");
+}
+
+#define CHECK_SUCESS(a,b){ a==b ? cout << "\033[1;32mSUCCESS\033[0m\n": cout << "\033[1;31mFAIL\033[0m\n;";}
+
+int main()
+{
+    List<bytes8> a;
+// initial values  
+    string s_over = "abcdefghijk";
+    string s_fit = "abcdefgh";
+    string s_under = "abcde";
+
+    bytes b_over(s_over.begin(), s_over.end());
+    bytes b_fit(s_fit.begin(), s_fit.end());
+    bytes b_under(s_under.begin(), s_under.end());
+
+    a.data().push_back(bytes8(b_over));
+    a.data().push_back(bytes8(b_fit));
+    a.data().push_back(bytes8(b_under));
+
+    bytes enc = a.to_bytes(0, little);
+    print_hex(enc);
+
+
+    uint256 testval1("0x1111111111111111111111111111111111111111111111111111111111111111");
+    uint256 testval2("0x2222222222222222222222222222222222222222222222222222222222222222");
+    uint256 testval3("0x3333333333333333333333333333333333333333333333333333333333333333");
+    List<uint256> b;
+    b.data().push_back(testval1);
+    b.data().push_back(testval2);
+    b.data().push_back(testval3);
+
+    bytes enc1 = b.to_bytes(0, little);
+    print_hex(enc1);
+    
+}
+
